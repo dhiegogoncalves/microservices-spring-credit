2495b7bcc3d76de5695d7b8f1be519304cc06ed3
Dhiego Gonçalves da Silva <dhhiego@gmail.com>
2022-08-22 14:41:06 -0400

    feat: cloud gateway created and configured.

---
+++ cloud-gateway/src/main/java/com/microservices/cloudgateway/CloudGatewayApplication.java
@@ -0,0 +1,28 @@
+package com.microservices.cloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+@EnableDiscoveryClient
+@EnableEurekaClient
+@SpringBootApplication
+public class CloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder) {
+		return builder
+				.routes()
+				.route(r -> r.path("/customers/**").uri("lb://customer-api"))
+				.build();
+	}
+
+}
